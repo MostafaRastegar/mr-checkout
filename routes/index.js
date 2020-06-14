db72813b5380aac72d69726581ebd790ce053a4a
mostafa rastegar <m.rastegar991@gmail.com>
2020-06-14 08:53:37 +0430

    Add item to success page

--- routes/index.js
+++ routes/index.js
@@ -16,40 +16,56 @@ router.get("/checkout/:orderId", function (req, res, next) {
 router.post("/payment/submit/", function (req, res, next) {
   const { orderId, amount } = req.body;
   // send request and redirect
-  const loginBankUrl = 'http://localhost:4002/login'
   const token = "youraccesstokensecret";
   const callBackUrl = `http://localhost:4001/success`;
 
-  var dataToPost = {
-    "userName": "john",
-    "userPassword": "password123admin",
-	  token
- };
+ res.render("payment-submit", {
+   title: "Payment submit",
+   orderId,
+   amount,
+   token: "youraccesstokensecret",
+   callBackUrl: "http://localhost:4001/success",
+   userName: "john",
+   userPassword: "password123admin",
+ });
 
- axios.post(loginBankUrl, dataToPost)
- .then((result) => {
-   const { status } = result.data;
-   if(status === 'ok'){
-       res.render("payment-submit", {
-         title: "Payment submit",
-         orderId,
-         token,
-         callBackUrl,
-         amount,
-       });
-   }
- })
- .catch((err) => {
-   console.log("error: ", err);
- })
+ // axios.post(loginBankUrl, dataToPost)
+ // .then((result) => {
+ //   const { status } = result.data;
+ //   if(status === 'ok'){
+ //       res.render("payment-submit", {
+ //         title: "Payment submit",
+ //         orderId,
+ //         token,
+ //         callBackUrl,
+ //         amount,
+ //       });
+ //   }
+ // })
+ // .catch((err) => {
+ //   console.log("error: ", err);
+ // })
 });
 
 router.post("/success", function (req, res, next) {
-  const { orderId, ResCode } = req.body;
-  if(ResCode === "0"){
-    res.render("payment-success", { title: "Thank you!", orderId });
+   const {
+     saleOrderId,
+     saleReferenceId,
+     refId,
+     resCode,
+   } = req.body;
+  if(resCode === "0"){
+    res.render("payment-success", {
+      title: "Thank you!",
+      result: {
+        saleOrderId,
+        saleReferenceId,
+        refId,
+        resCode,
+      }
+    });
   }
-  if(ResCode === "17"){
+  if(resCode === "17"){
     res.redirect('/failed');
   }
 });
